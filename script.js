c1a0a83f4ab317739343e00300fc7fc872b614c7
Vaishali0903 <114869269+Vaishali0903@users.noreply.github.com>
2023-01-04 10:36:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let btn = document.querySelector(".toggle");
+let icon = btn.querySelector(".fa-bars");
+
+btn.onclick = function(){
+if(icon.classList.contains("fa-bars")){
+    icon.classList.replace("fa-bars", "fa-xmark");
+}
+else{
+    icon.classList.replace("fa-xmark","fa-bars");
+}
+}
